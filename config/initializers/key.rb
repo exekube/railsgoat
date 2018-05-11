06d8a81438e943af4eae2ec93b21da0b3c314189
Ilya Sotkov <ilya@sotkov.com>
2018-05-11 06:57:20 +0300

    Add .dockerignore and default KEY value in produciton (security risk)

--- config/initializers/key.rb
+++ config/initializers/key.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 if Rails.env.production?
-  KEY = ENV.fetch("KEY")
+  KEY = ENV.fetch("KEY") { "123456789101112123456789101112123456789101112" }
   # Specify env variable/location/etc. to retrieve key from
 else
   KEY = "123456789101112123456789101112123456789101112"
